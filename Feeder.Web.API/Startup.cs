e1c225223cd94ea8002d61bd5bc2f028c4034b9a
Mahmoud <m.moghni99@gmail.com>
2018-10-19 14:34:18 +0200

    Configure Swagger

--- Feeder.Web.API/Startup.cs
+++ Feeder.Web.API/Startup.cs
@@ -9,6 +9,7 @@
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Swashbuckle.AspNetCore.Swagger;
 
     public class Startup
     {
@@ -27,7 +28,24 @@
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             var connection = Configuration["ConnectionStrings:FeederDbConnection"];
-            services.AddDbContext<FeederContext>( options => options.UseSqlServer(connection));
+            services.AddDbContext<FeederContext>(options => options.UseSqlServer(connection));
+
+            services.AddMvc(setupAction =>
+           {
+               setupAction.ReturnHttpNotAcceptable = true;
+           });
+
+            services.AddSwaggerGen(c =>
+           {
+               c.SwaggerDoc("v1",
+                   new Info
+                   {
+                       Version = "v1",
+                       Title = "Feeder API",
+                       Description = "Feeder - APIs documentation",
+                       TermsOfService = "none"
+                   });
+           });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,12 +55,15 @@
             {
                 app.UseDeveloperExceptionPage();
             }
-            //else
-            //{
-            //    app.UseHsts();
-            //}
 
-            //app.UseHttpsRedirection();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+           {
+               c.SwaggerEndpoint("../swagger/v1/swagger.json", "Feeder API V1");
+               c.RoutePrefix = "docs";
+           });
+
             app.UseMvc();
         }
 
