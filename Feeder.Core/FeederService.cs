9a23a5d435ed3692482bc82efa9a391985230147
Mahmoud <m.moghni99@gmail.com>
2018-10-21 13:25:26 +0200

    Edit in Feeder. Core

---
+++ Feeder.Core/FeederService.cs
@@ -0,0 +1,62 @@
+﻿namespace Feeder.Core
+{
+    using Feeder.Data.Entities;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Xml.Linq;
+    using System.Linq;
+    using System;
+
+    public class FeederService : IFeederService
+    {
+        public T GetFeedsAsync<T>(string feedUri, Feed.SourceType source)
+        {
+            switch(source)
+            {
+                case Feed.SourceType.RSS:
+                    return (T) Convert.ChangeType(GetRssFeeds(feedUri), typeof(T));
+
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private List<Item> GetRssFeeds(string feedUri)
+        {
+            var webClient = new WebClient();
+
+            var response = webClient.DownloadString(feedUri);
+
+            XDocument document = XDocument.Parse(response);
+
+            var items = (from descendant in document.Descendants("item")
+                         select new Item
+                         {
+                             Description = descendant.Element("description").Value,
+                             Title = descendant.Element("title").Value,
+                             PublishDate = descendant.Element("pubDate").Value
+
+                         }).ToList();
+
+            return items;
+        }
+
+        public bool IsValidRssUri(string feedUri)
+        {
+            try
+            {
+                var webClient = new WebClient();
+
+                var response = webClient.DownloadString(feedUri);
+
+                XDocument document = XDocument.Parse(response);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Feeder.Core/IFeederService.cs
@@ -0,0 +1,13 @@
+﻿namespace Feeder.Core
+{
+    using Feeder.Data.Entities;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IFeederService
+    {
+        T GetFeedsAsync<T>(string feedUri, Feed.SourceType source);
+
+        bool IsValidRssUri(string feedUri);
+    }
+}
